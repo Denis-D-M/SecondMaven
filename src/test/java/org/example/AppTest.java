a39bc0ef4b74fd6c45c172d9b1de448fcec20966
Denis Mishin <Denis_Mishin@epam.com>
2021-02-17 14:59:35 +0400

    Создал SecondMaven. Просто доп звено в цепочке.

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import org.junit.*;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
